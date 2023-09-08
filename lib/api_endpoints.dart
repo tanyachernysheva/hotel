9d1825bef06dd63629e8cf73eb2aedae285a9c05
Tanya Chernysheva <chernysheva.gg@gmail.com>
2023-09-08 14:07:18 +0300

    apiEndpoints

---
+++ lib/api_endpoints.dart
@@ -0,0 +1,10 @@
+abstract class ApiEndpoints {
+  static const hotelUrl =
+      'https://run.mocky.io/v3/35e0d18e-2521-4f1b-a575-f0fe366f66e3';
+
+  static const roomUrl =
+      'https://run.mocky.io/v3/f9a38183-6f95-43aa-853a-9c83cbb05ecd';
+
+  static const bookingUrl =
+      'https://run.mocky.io/v3/e8868481-743f-4eb2-a0d7-2bc4012275c8';
+}
